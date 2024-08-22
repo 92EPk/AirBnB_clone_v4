5bf7ed0817c74ff62c3373da7822b4d9ebcafd4e
Bx <53805165+92EPk@users.noreply.github.com>
2024-08-22 18:13:01 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.operations import run, put, sudo, local
 from datetime import datetime
 import os
 
-env.hosts = ['66.70.184.249', '54.210.138.75']
+env.hosts = ['54.227.222.93', '34.207.57.19']
 created_path = None
 
 
